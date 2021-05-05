f5875b74063018e5cb35cee2f37814b96e03c9f6
You Song Hou <yousonghou@yahoo.com>
2021-05-05 19:45:21 -0400

    add config to deploy contracts on SKALE

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,18 @@
+/*
+ * This truffle script will deploy your smart contracts to your SKALE Chain.
+ *
+ *  @param {String} privateKey - Provide your wallet private key.
+ *  @param {String} provider - Provide your SKALE endpoint address.
+ */
+require("dotenv").config();
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+//https://skale.network/developers/ for SKALE documentation
+//Provide your wallet private key
+const privateKey = process.env.PRIVATEKEY;
+//Provide your SKALE endpoint address
+const skale = process.env.SKALE;
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +20,12 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    // truffle deploy --reset --network skale --compile-all
+    skale: {
+      provider: () => new HDWalletProvider(privateKey, skale),
+      gasPrice: 0,
+      network_id: "*"
+    }
   },
   contracts_build_directory: './src/abis/',
   compilers: {
