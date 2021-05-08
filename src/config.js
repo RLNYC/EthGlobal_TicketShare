2a892dfefca40d4c511adf5a00c7fbf6b8606997
You Song Hou <yousonghou@yahoo.com>
2021-05-07 20:11:39 -0400

    remove config.js

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const SKALE_CHAIN_ENDPOINT = "https://dev-testnet-v1-1.skalelabs.com";
-export const ACCOUNT_ADDRESS = "0xb0760543e7Db8ED15AaCcF8d6C89ce78B0cA7Aba";
-export const YOUR_PRIVATE_KEY = "74b955c0db6d04d04a3c657da6e1c53de24f7b4d01790c5c7627ac07fc53a59b";
